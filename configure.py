f9b007506751c33f6e61cdbd8214ae3df00df756
Buildbot user <buildbot@openafs.mit.edu>
2018-08-06 22:42:47 -0400

    Less configure variables

--- configure.py
+++ configure.py
@@ -1,13 +1,6 @@
 from os import getcwd
 from os.path import abspath, join
 
-topdir = getcwd()
-buildbot = abspath(join(topdir, '../venv/bin/buildbot'))
-master = abspath(join(topdir, '../master'))
-makefile = \
-    open('Makefile.in')\
-    .read()\
-    .replace('@TOPDIR@', topdir)\
-    .replace('@BUILDBOT@', buildbot)\
-    .replace('@MASTER@', master)
+topdir = abspath(join(getcwd(), '..'))
+makefile = open('Makefile.in').read().replace('@TOPDIR@', topdir)
 open('Makefile', 'w').write(makefile)
