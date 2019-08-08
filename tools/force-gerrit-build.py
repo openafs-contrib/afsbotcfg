8dbeb1ceee65225932cc6a24bfffeafa49f601b6
Michael Meffie <mmeffie@sinenomine.net>
2019-08-08 13:18:30 -0400

    support force-gerrit-build rc file
    User proper command line handling and support an rc file for username
    and password values.

--- tools/force-gerrit-build.py
+++ tools/force-gerrit-build.py
@@ -1,34 +1,61 @@
+#!/usr/bin/python3
 
 import requests
 import sys
+import os
+import argparse
+from configparser import ConfigParser
 
-username = sys.argv[1]
-password = sys.argv[2]
-changenumber = sys.argv[3]
+def die(msg):
+    sys.stderr.write('%s\n' % msg)
+    sys.exit(1)
 
-payload = {
-  "id": 1,
-  "jsonrpc": "2.0",
-  "method": "force",
-  "params": {
-    "builderid": "2",
-    "username": "",
-    "reason": "Force Gerrit Build",
-    "branch": "",
-    "project": "test",
-    "repository": "",
-    "revision": "",
-    "changenumber": changenumber,
-    "patchsetnumber": ""
-  }
-}
+def main():
+    config = ConfigParser()
+    config.read([os.path.expanduser('~/.buildbotrc')])
 
-session = requests.Session()
-session.auth = (username, password)
-auth = session.get('https://buildbot.openafs.org/auth/login')
-print('auth:', auth)
-print(auth.text)
+    parser = argparse.ArgumentParser(
+        description='Force Gerrit Builds',
+        epilog='Default values are read from the ~/.buildbotrc file, if it exists.')
+    parser.add_argument('-u', '--username', metavar='<username>', default=config.get('login','username',fallback=None))
+    parser.add_argument('-p', '--password', metavar='<password>', default=config.get('login','password',fallback=None))
+    parser.add_argument('number', type=int)
+    args = parser.parse_args()
+    if not args.username:
+        die('No username specified.')
+    if not args.password:
+        die('No password specified.')
 
-rsp = session.post('https://buildbot.openafs.org/api/v2/forceschedulers/ForceGerritBuild', json=payload)
-print('rsp:', rsp)
-print(rsp.text)
+    payload = {
+      'id': 1,
+      'jsonrpc': '2.0',
+      'method': 'force',
+      'params': {
+        'builderid': '2',
+        'username': '',
+        'reason': 'Force Gerrit Build',
+        'branch': '',
+        'project': 'test',
+        'repository': '',
+        'revision': '',
+        'changenumber': '%d' % args.number,
+        'patchsetnumber': ''
+      }
+    }
+
+    session = requests.Session()
+    session.auth = (args.username, args.password)
+    print('Logging in as %s' % args.username)
+    auth = session.get('https://buildbot.openafs.org/auth/login')
+    if auth.status_code != 200:
+        print('Login failed', auth.text)
+        return 1
+    print('Login ok')
+
+    rsp = session.post('https://buildbot.openafs.org/api/v2/forceschedulers/ForceGerritBuild', json=payload)
+    print(rsp)
+    print(rsp.text)
+    return rsp.status_code == 200
+
+if __name__ == '__main__':
+    sys.exit(main())
