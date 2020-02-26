daba9edf68259a39f81a6dcd0479698b54cdedc0
Michael Meffie <mmeffie@sinenomine.net>
2020-02-26 16:01:48 -0500

    roll-call: add email notification
    To make it easier to monitor offline workers.

--- tools/bb-roll-call.py
+++ tools/bb-roll-call.py
@@ -10,6 +10,15 @@
 import argparse
 import sys
 import requests
+import smtplib
+import ssl
+import os
+from email.message import EmailMessage
+from email.utils import formatdate, make_msgid
+try:
+    from configparser import ConfigParser
+except ImportError:
+    from ConfigParser import ConfigParser
 
 WORKERS = (
     'centos73-x86_64',
@@ -44,20 +53,20 @@ WORKERS = (
     'win7-amd64',
 )
 
-class RCError(Exception):
+class RollCallError(Exception):
     pass
 
-def print_worker(w):
-    fields = (w['status'], w['name'], w['admin'], w['host'])
-    sep = ':'
-    print(sep.join(fields))
+def roll_call():
+    """Retreive connection status of workers.
 
-def roll_call(showall=False):
-
+    Returns an dictionary of workers by name. The values include the connection
+    status and worker admin info.  Raises exceptions if the workers info cannot
+    be retreive, an entry is missing, or an unexcepted entry is found.
+    """
     session = requests.Session()
     rsp = session.get('https://buildbot.openafs.org/api/v2/workers')
     if rsp.status_code != 200:
-        raise RCError('GET failed: %s' % rsp.text)
+        raise RollCallError('Request failed: %s' % rsp.text)
 
     workers = {}
     for worker in rsp.json()['workers']:
@@ -73,40 +82,77 @@ def roll_call(showall=False):
             host = ''
         admin = admin.replace('\n', ' ').replace(':', ' ').strip()
         host = host.replace('\n', ' ').replace(':', ' ').strip()
+
+        # Verify we know about this worker if it is online.
+        if name not in WORKERS:
+            if status == 'online':
+                raise RollCallError('Unknown worker: %s' % name)
+            else:
+                continue # Ingore offline old workers. Buildbot keeps these around.
+
         workers[name] = {'name':name, 'status':status, 'admin':admin, 'host':host}
 
-    # List the status of the known workers. The returned list may also contain
-    # old workers (which should be offine).
-    num_offline = 0
-    for name in sorted(WORKERS):
-        w = workers.get(name)
-        if w is None:
-            raise RCError('Missing worker: %s' % name)
-        if showall:
-            print_worker(w)
-        elif w['status'] == 'offline':
-            print_worker(w)
-        if w['status'] == 'offline':
-            num_offline += 1
+    # Verify we did not miss any.
+    for name in WORKERS:
+        if not name in workers:
+            raise RollCallError('No information for worker: %s' % name)
 
-    # Be sure our list is up to date.
-    for name, worker in workers.items():
-        if worker['status'] == 'online' and not name in WORKERS:
-            raise RCError('Unknown worker is online: %s' % name)
+    return workers
 
-    return num_offline
+def as_text(workers, all_=True):
+    """Format the output as a text string."""
+    lines = []
+    for name in sorted(workers.keys()):
+        w = workers[name]
+        if all_ or w['status'] == 'offline':
+            line = '%-8s %-28s %s' % (w['status'], w['name'], w['admin'])
+            lines.append(line)
+    return '\n'.join(lines)
+
+def sendmail(subject, text):
+    """Send an email."""
+    parser = ConfigParser()
+    parser.read([os.path.expanduser('~/.buildbotrc')])
+    try:
+        c = dict(parser.items('email'))
+    except KeyError:
+        raise RollCallError('Missing email section in .buildbotrc')
+    for name in ('server', 'port', 'userid', 'password', 'from', 'to'):
+        if not name in c:
+            raise RollCallError('Missing email %s option in .buildbotrc' % name)
+
+    msg = EmailMessage()
+    msg['From'] = c['from']
+    msg['To'] = c['to'].split(',')
+    msg['Subject'] = subject
+    msg['Date'] = formatdate()
+    msg['Message-ID'] = make_msgid()
+    msg.set_content(text)
+
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(c['server'], c['port'], context=context) as server:
+        server.login(c['userid'], c['password'])
+        server.send_message(msg)
 
 def main():
     parser = argparse.ArgumentParser(description='Take worker attendance')
     parser.add_argument('-a', '--all', action='store_true', help='Show all workers')
+    parser.add_argument('-m', '--mail', action='store_true', help='Send email')
     args = parser.parse_args()
     try:
-        count = roll_call(showall=args.all)
-    except RCError as e:
-        sys.stderr.write('Error: %s\n' % e)
+        workers = roll_call()
+        text = as_text(workers, all_=args.all)
+        if text:
+            sys.stdout.write('%s\n' % text)
+            if args.mail:
+                sendmail('Buildbot roll call', text)
+    except RollCallError as e:
+        text = 'Error: %s\n' % e
+        sys.stderr.write(text)
+        if args.mail:
+            sendmail('Buildbot roll call error', text)
         return 1
-    rc = 1 if count > 0 else 0
-    return rc
+    return 0
 
 if __name__ == '__main__':
     sys.exit(main())
