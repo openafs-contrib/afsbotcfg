6fad4a1e446d3d1d51b175e966d1a8159e87f0c6
Michael Meffie <mmeffie@sinenomine.net>
2021-05-07 12:57:28 -0400

    Add limit_gerrit_builds setting
    Since the gerrit verification is all-or-none, sometimes we want to
    temporarily remove builders from the gerrit pool. At the moment we have
    two masters (a prod and one being staged to take over), so the list may
    be diffent depending on the host.
    
    To support this, add a master.cfg setting and ansible host variable
    called 'limit_gerrit_builders' that has a list of which builders are
    gerrit builders.

--- files/settings.py
+++ files/settings.py
@@ -2,3 +2,12 @@
 
 buildbotURL = '{{ buildbot_master_url }}'
 www_port = {{ buildbot_master_www_port }}
+{% if afsbotcfg_limit_gerrit_builders is undefined %}
+limit_gerrit_builders = None
+{% else %}
+limit_gerrit_builders = [
+{% for name in afsbotcfg_limit_gerrit_builders %}
+    '{{ name }}',
+{% endfor %}
+]
+{% endif %}
