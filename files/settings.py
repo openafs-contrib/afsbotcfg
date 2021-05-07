9f7a810c99dde3dd5d256c6cb5c46e2dffbd3f51
Michael Meffie <mmeffie@sinenomine.net>
2021-05-07 13:25:15 -0400

    Update playbook host var names
    The www url and port values are playbook level variables, not role
    variables, so prefix them with afsbotcfg_ instead of buildbot_master_.

--- files/settings.py
+++ files/settings.py
@@ -1,7 +1,8 @@
 # OpenAFS Buildbot master site specific settings.
 
-buildbotURL = '{{ buildbot_master_url }}'
-www_port = {{ buildbot_master_www_port }}
+title = '{{ afsbotcfg_title }}'
+buildbotURL = '{{ afsbotcfg_url }}'
+www_port = {{ afsbotcfg_www_port }}
 {% if afsbotcfg_limit_gerrit_builders is undefined %}
 limit_gerrit_builders = None
 {% else %}
