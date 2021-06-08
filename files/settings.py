886b9c22d26ae80870bc9db6deaabb6730fee2f6
Michael Meffie <mmeffie@sinenomine.net>
2021-06-08 13:36:34 -0400

    Fix links for the test builds on the new buildbot
    The url for the new buildbot is not ready yet, so dont publish
    links yet.
    
    Also, delay the test build on the new buildbot so the old buildbot can
    verify first.

--- files/settings.py
+++ files/settings.py
@@ -3,6 +3,7 @@
 title = '{{ afsbotcfg_title }}'
 buildbotURL = '{{ afsbotcfg_url }}'
 www_port = {{ afsbotcfg_www_port }}
+build_delay = {{ afsbotcfg_build_delay | d(120) }}
 {% if afsbotcfg_limit_gerrit_builders is undefined %}
 limit_gerrit_builders = None
 {% else %}
