f80a395d4e871704a79eab880b38d2b9565650f9
Michael Meffie <mmeffie@sinenomine.net>
2021-08-05 11:30:52 -0400

    Remove limit gerrit builders feature
    This was used during the transition to the new buildbot and is
    no longer used.

--- files/settings.py
+++ files/settings.py
@@ -5,12 +5,3 @@ repo = '{{ afsbotcfg_repo }}'
 buildbotURL = '{{ afsbotcfg_url }}'
 www_port = {{ afsbotcfg_www_port }}
 build_delay = {{ afsbotcfg_build_delay | d(120) }}
-{% if afsbotcfg_limit_gerrit_builders is undefined %}
-limit_gerrit_builders = None
-{% else %}
-limit_gerrit_builders = [
-{% for name in afsbotcfg_limit_gerrit_builders %}
-    '{{ name }}',
-{% endfor %}
-]
-{% endif %}
