ab0b3ba8a57ffa99dfcacb78fd11f1f3ec6d16ce
Cheyenne Wills <cwills@sinenomine.net>
2022-04-12 09:46:52 -0600

    Remove settings.py, set cfg via ansible
    Update master.cfg to just use ansible to define the settings instead
    of importing settings.py
    
    Remove the settings.py file

--- files/settings.py
+++
@@ -1,8 +0,0 @@
-# OpenAFS Buildbot master site specific settings.
-
-title = '{{ afsbotcfg_title }}'
-summary_title = '{{ afsbotcfg_summary_title }}'
-repo = '{{ afsbotcfg_repo }}'
-buildbotURL = '{{ afsbotcfg_url }}'
-www_port = {{ afsbotcfg_www_port }}
-build_delay = {{ afsbotcfg_build_delay }}
