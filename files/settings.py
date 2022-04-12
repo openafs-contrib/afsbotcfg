6ed1030ed3a0adcd6a59d8020542a62a658751be
Cheyenne Wills <cwills@sinenomine.net>
2022-04-12 09:38:37 -0600

    Set WEB UI strings via ansible
    The title for the top banner on the web page and the url link are
    hardcoded strings.  In addition the settings.title is really being used
    as the string for in gerrit summary message.
    
    Set the WEB UI via ansible variables.
    Add a new settings that contains the gerrit summary message title.
    
    settings.title is the string that is displayed at the top of the
    buildbot web page (and is limited in length by buildbot).
    
    settings.summary_title is the string that is displayed in the build
    summary message sent back to gerrit.

--- files/settings.py
+++ files/settings.py
@@ -1,6 +1,7 @@
 # OpenAFS Buildbot master site specific settings.
 
 title = '{{ afsbotcfg_title }}'
+summary_title = '{{ afsbotcfg_summary_title }}'
 repo = '{{ afsbotcfg_repo }}'
 buildbotURL = '{{ afsbotcfg_url }}'
 www_port = {{ afsbotcfg_www_port }}
