09d954ad3d95a96bef8f3c648840f101892c58da
Michael Meffie <mmeffie@sinenomine.net>
2024-02-24 16:39:44 -0500

    Fix step name

--- src/afsbotcfg/factory.py
+++ src/afsbotcfg/factory.py
@@ -84,7 +84,7 @@ class UnixBuildAndTest(UnixBuild):
             self.addStep(afsbotcfg.steps.BuildTests(make=make))
             self.addStep(afsbotcfg.steps.VerboseRunTestsStep())
         else:
-            self.addStep(afsbotcfg.steps.CheckStep(make=make))
+            self.addStep(afsbotcfg.steps.Check(make=make))
 
 
 class UnixBuildDocs(Checkout):
