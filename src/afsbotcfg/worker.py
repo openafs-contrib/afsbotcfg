7d28674ea8c40032fe85dfebcd6cef7a08f3d049
Michael Meffie <mmeffie@sinenomine.net>
2025-01-27 12:05:32 +0100

    afsbotcfg: Add canStartBuild function to fallback to dummy workers

--- src/afsbotcfg/worker.py
+++ src/afsbotcfg/worker.py
@@ -10,7 +10,35 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 
+from twisted.python import log
+
 from buildbot.worker.local import LocalWorker
+
+
+def canStartBuild(builder, workerforbuilder, buildrequest):
+    workername = workerforbuilder.worker.workername
+
+    # Enforce selected worker if choosen in force build form.
+    if 'workername' in buildrequest.properties:
+        chosen = buildrequest.properties['workername']
+        if isinstance(chosen, str) and chosen == workername:
+            return True
+
+    # The dummy worker is a fallback to skip builds when no real workers
+    # are attached. The builder.workers list contains all of the currently
+    # attached workers, including workers that are busy doing a build.
+    if workername == 'dummy':
+        attached_real_workers = []
+        for wfb in builder.workers:
+            if wfb.worker.workername != 'dummy':
+                attached_real_workers.append(wfb.worker.workername)
+        if len(attached_real_workers) == 0:
+            log.msg(f"afsbotcfg: canStartBuild: no real workers found, skipping build {buildrequest.id}")
+            return True
+        return False
+
+    # Proceed with real worker.
+    return True
 
 
 class DummyWorker(LocalWorker):
